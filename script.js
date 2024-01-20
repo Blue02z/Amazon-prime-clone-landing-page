184d928d7af5ba9f41bcb25fd4f47c2befe213de
Marcus Vinicius <102832530+Blue02z@users.noreply.github.com>
2024-01-19 22:00:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+ 
+
+
+function inicioMenu(){
+    let inicio = document.querySelector(".inicio-menu")
+    inicio.classList.add("open")
+}
+
+function inicioMenuSair(){
+    inicio.classList.remove("open")
+
+}
+
+function inicioMenu2(){
+    let inicio2 = document.querySelector(".inicio-menu2")
+    inicio2.classList.add("open")
+    
+}
+
+function inicioMenuSair2(){
+    inicio2.classList.remove("open")
+}
+
+function inicioMenu4(){
+    let inicio4 = document.querySelector(".inicio-menu4")
+    inicio4.classList.add("open")
+    
+}
+
+function inicioMenuSair4(){
+    inicio4.classList.remove("open")
+}
+
+function inicioMenu3(){
+    let inicio3= document.querySelector(".inicio-menu3")
+    inicio3.classList.add("open")
+    
+}
+
+function inicioMenuSair3(){
+    inicio3.classList.remove("open")
+}
